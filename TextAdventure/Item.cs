9da085347eb33f28310ef5a50e67906842e90418
William Terry <Tezza48is@aol.com>
2016-01-14 22:17:12 +0000

    new item subclasses
    take now checks that an item isn't a Lock subobject instead of an
    enumerable Lock

--- TextAdventure/Item.cs
+++ TextAdventure/Item.cs
@@ -9,28 +9,10 @@ namespace TextAdventure
 
 	class Item
 	{
-
-        public enum ItemType
-        {
-            Item, Key, Component
-        };
-
-        private string itemName;
-        private string itemDescription;
-        private bool isTakeable = false; // can this item be taken
-        private Item itemUsedWith; 
-        private Item alternateItem;
-        private LockedExit pairedLockedExit = null; // locked exit this is paired to if this item can become an exit 
+        protected string itemName;
+        protected string itemDescription;
 
         public string ItemDescription { get { return itemDescription; } }
-
-        public bool IsTakeable { get { return isTakeable; } set { isTakeable = value; } }
-
-        internal Item ItemUsedWithName { get { return itemUsedWith; } set { itemUsedWith = value; } }
-
-        internal Item AlternateItem { get { return alternateItem; } set { alternateItem = value; } }
-
-        internal LockedExit PairedLockedExit { get { return pairedLockedExit; } set { pairedLockedExit = value; } }
 
         public Item()
 		{
@@ -50,28 +32,144 @@ namespace TextAdventure
             itemDescription = description;
         }
 
-        public Item(string name, bool takeable)
-        {
-            itemName = name;
-            itemDescription = "";
-            isTakeable = takeable;
-        }
-
-        public Item(string name, string description, bool takeable)
-        {
-            itemName = name;
-            itemDescription = description;
-            isTakeable = takeable;
-        }
-
         public override string ToString()
         {
             return itemName.ToLower();
         }
+    }
 
-        public void addUse()
+    class Tool : Item
+    {
+        /*
+            A tool must:
+                have an item it's used on
+        */
+
+        private Craftable itemUsedOn;
+
+        public Tool()
         {
+            itemName = "";
+            itemDescription = "";
+            itemUsedOn = null;
+        }
 
+        public Tool(string name, string description, Craftable _itemUsedOn)
+        {
+            itemName = name;
+            itemDescription = description;
+            itemUsedOn = _itemUsedOn;
+        }
+    }
+
+    class Multitool : Tool
+    {
+        // can be used on multiple craftables
+        private List<Craftable> itemsUsedOn;
+
+        public Multitool()
+        {
+            itemName = "";
+            itemDescription = "";
+            itemsUsedOn = new List<Craftable>();
+        }
+
+        public Multitool(string name, string description)
+        {
+            itemName = name;
+            itemDescription = description;
+            itemsUsedOn = new List<Craftable>();
+        }
+
+        public Multitool (string name, string description, List<Craftable> _itemsUsedOn)
+        {
+            itemName = name;
+            itemDescription = description;
+            itemsUsedOn = _itemsUsedOn;
+        }
+    }
+
+    class Craftable : Item
+    {
+        /*
+            A craftable must:
+                turn into another item when it's tool is used on it
+        */
+        private Item craftedItem;
+
+        public Craftable()
+        {
+            itemName = "";
+            itemDescription = "";
+        }
+
+        public Craftable(string name, string description, Item _craftedItem)
+        {
+            itemName = name;
+            itemDescription = description;
+            craftedItem = _craftedItem;
+        }
+
+        public Item Craft()
+        {
+            return craftedItem;
+        }
+    }
+
+    class Key : Item
+    {
+        /*
+            A key must:
+                know if it's destroyed on use
+        */
+
+        private bool isDestroyedOnUse;
+
+        public Key()
+        {
+            itemName = "";
+            itemDescription = "";
+            isDestroyedOnUse = true;
+        }
+
+        public Key(string name, string description, bool _isDestroyedOnUse)
+        {
+            itemName = name;
+            itemDescription = description;
+            isDestroyedOnUse = _isDestroyedOnUse;
+        }
+    }
+
+    class Lock : Item
+    {
+        /*
+            A losk must:
+                have a key
+                unlock a locked exit
+                turn into another item when unlocked
+                    (an unlocked version of it'self)
+        */
+
+        private Key key;
+        private Item unlockedVersion;
+        private LockedExit lockedExit;
+
+        public Lock()
+        {
+            itemName = "";
+            itemDescription = "";
+            key = null;
+            unlockedVersion = null;
+            lockedExit = null;
+        }
+
+        public Lock(string name, string description, Key _key, Item _unlockedVersion, LockedExit _lockedExit)
+        {
+            itemName = name;
+            itemDescription = description;
+            key = _key;
+            unlockedVersion = _unlockedVersion;
+            lockedExit = _lockedExit;
         }
     }
 }
